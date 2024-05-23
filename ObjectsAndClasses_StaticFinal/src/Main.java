9a332f544378fbd4204654a7efe959ea5837ad3a
Hamid Musayev <cherryducker@gmail.com>
2024-05-23 23:39:11 +0400

    Второе задание модуля 6, первая попытка.

---
+++ ObjectsAndClasses_StaticFinal/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Computer computerOne = new Computer("ASUS", "AMD");
+        System.out.println(computerOne.toString());
+
+        Computer computerTwo = new Computer("Dell", "Nvidia");
+        System.out.println(computerOne.toString());
+
+        Computer computerThree = new Computer("Lenovo", "AMD");
+        System.out.println(computerOne.toString());
+
+
+
+    }
+}
