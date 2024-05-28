13bd36924ca7b9af52da67df5b9a1ff51aa50f30
Hamid Musayev <cherryducker@gmail.com>
2024-05-28 13:39:42 +0400

    Задание 7го модуля, первая попытка

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -4,6 +4,23 @@ public class Main {
         container.addCount(5672);
         System.out.println(container.getCount());
 
+        for (char i = 1; i <= 'ё'; i++) {
+
+            if (i >= 'А') {
+                if (i == 'ѐ') {
+                    continue;
+                }
+
+                System.out.println("Номером буквы " + i + " является: " + (int) i);
+            }
+            if (i == 'Ё') {
+                System.out.println("Номером буквы " + i + " является: " + (int) i);
+
+            }
+
+
+        }
+
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
